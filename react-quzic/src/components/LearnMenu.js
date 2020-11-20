18270e7b1b0c626d704bc7fc6c450e490a032454
JommieJom <jombhanut@gmail.com>
2020-11-20 13:39:46 +0700

    update learn menu

--- react-quzic/src/components/LearnMenu.js
+++ react-quzic/src/components/LearnMenu.js
@@ -7,41 +7,61 @@ function LearnMenu() {
         <div className="learn">
             <h1>Learning!</h1>
             <div className="learn_container">
-                <div className="learn_wrapper"> 
+                <div className="learn_wrapper">
                     <ul className="learn_items">
-                        <LearnItem 
-                        src="images/sightReading2.jpg" 
-                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                        label='Music Note Sight Reading'
-                        path='/' 
+                        <LearnItem
+                            src="images/reading.png"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                            label='The Basics'
+                            path='/'
                         />
-                        <LearnItem 
-                        src="images/slogan3.png" 
-                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                        label='Rythm'
-                        path='/' 
+                        <LearnItem
+                            src="images/intervals.png"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                            label='Rythm and meter'
+                            path='/'
+                        />
+
+                    </ul>
+
+                    <ul className="learn_items">
+                        <LearnItem
+                            src="images/scales.png"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                            label='Scales'
+                            path='/'
+                        />
+                        <LearnItem
+                            src="images/learnchords.png"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. ipsum dolor sit amet, consectetur adipiscing elit."
+                            label='Chord Theory'
+                            path='/'
+                        />
+
+
+                    </ul>
+
+
+
+                    <ul className="learn_items">
+
+                        <LearnItem
+                            src="images/learnharmonics.png"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. ipsum dolor sit amet, consectetur adipiscing elit."
+                            label='Harmonic Functions'
+                            path='/'
+                        />
+                        <LearnItem
+                            src="images/perfectPitch3.jpg"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                            label='Perfect Pitch'
+                            path='/'
                         />
                     </ul>
-            
-                    <ul className="learn_items">
-            
-                        <LearnItem 
-                        src="images/Chords2.jpg" 
-                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. ipsum dolor sit amet, consectetur adipiscing elit."
-                        label='Chord Theory'
-                        path='/' 
-                        />
-                        <LearnItem 
-                        src="images/perfectPitch3.jpg" 
-                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                        label='Perfect Pitch'
-                        path='/' 
-                        />
-                    </ul>
-                
+
                 </div>
             </div>
-            
+
         </div>
     )
 }
